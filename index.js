29c0803357f0bbb41e2726a61859ec329637fefb
Richard <richard.robins@gmail.com>
2017-03-13 14:13:58 -0600

    everything but time stamp and black diamond

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//const cors = require('cors');
 
+const app = express();
+const port = 3000;
+let messages = [];
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', (req, res, next) =>{
+  res.status(200).send({messages: messages})
+});
+
+app.post('/messages', (req, res, next) =>{
+  messages.push({message: req.body.message, time: new Date()});
+  res.status(200).send({messages: messages});
+
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
